3cd3e8bc653273aec2b2f7aecb54dab3b69d9216
Kenneth <kennethyoung.2960@gmail.com>
2024-05-29 18:33:32 -0700

    Added error handlers and bones for routes

--- src/app.js
+++ src/app.js
@@ -4,10 +4,18 @@ const getZoos = require('./utils/getZoos');
 
 const app = express();
 
-
+app.get('/check/:zip', validateZip, (req, res) => {
+    // add if else for zoo exists
+});
 
 // Error handler middleware
+app.use((req, res, next) => {
+    res.status(404).send('That route could not be found');
+});
 
 // Not Found error handler middleware
+app.use((err, req, res, next) => {
+    res.status(500).send('There was an internal server error.');
+});
 
 module.exports = app;
